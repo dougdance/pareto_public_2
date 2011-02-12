52717db94e8a92a3e367c7c2485f3e99a7c85760
Doug <dougdance@gmail.com>
2011-02-12 11:30:42 -0800

    initial commit - a working refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParetoPublic2::Application.config.secret_token = 'a770727f3bfedc6f0f55d901c2ee658cd1d7a8cbbfe1240ae60858447393a2c813fbe2722ad4ca8c0396d8975c75d8dfca3a9bf945b051e94047cd9a02feadea'
